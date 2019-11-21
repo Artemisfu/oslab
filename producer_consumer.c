42476dca764f40ac92d4b9b819ee1cd36f294674
Daisy <daisy@localhost.localdomain>
2019-11-21 16:10:07 +0800

    update

--- producer_consumer.c
+++ producer_consumer.c
@@ -3,12 +3,14 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <time.h>
 #define MAX_PRODUCT 15
 #define PRODUCTOR 5
-#define CONSUMER 3
-pthread_mutex_t mutex;
+#define CONSUMER 3 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 sem_t empty_sem;
 sem_t full_sem;
+time_t tm;
 int in = 0;
 int out = 0;
 int product[MAX_PRODUCT];
@@ -20,7 +22,7 @@ int findEmpty(int*product){
     }
     return -1;
 }
-int findNotEmpty(int*product){
+int findFull(int*product){
     for(int i=0;i<MAX_PRODUCT;i++){
         if(product[i]!=0){
             return i;
@@ -28,23 +30,67 @@ int findNotEmpty(int*product){
     }
     return -1;
 }
-void *productor(){
-
-    sem_wait(empty_sem);
-    usleep(rand(100)*50000);
-    int i=findEmpty(product);
-    if(i!=-1){
-        product[i]=1;
+void printProduct(int *product){
+    for(int i=0;i<MAX_PRODUCT;i++){
+        if(product[i]==0){
+            printf("□");
+        }
+        else{
+            printf("■")
+        }
     }
-    
-    
+    printf("\n");
+}
+void *productor(int productor_id){
+    while(true){
+        printf("生产者%d试图进入缓冲区\n", productor_id);
+        pthread_mutex_lock(&mutex);
+        printf("生产者%d进入缓冲区\n", productor_id);
+        sem_wait(empty_sem);
+        usleep(rand(100)*50000);
+        int i=findEmpty(product);
+        if(i!=-1){
+            printProduct(product);
+            product[i]=productor_id;
+            printf("生产者%d在第%s个位置生产了产品\n", productor_id, i);
+            printProduct(product);
+        }
+        else{
+            sem_post(full_sem);
+        }
+    }
 }
 void *consumer(){
-
+    while(true){
+        printf("消费者者%d试图进入缓冲区\n", consumer_id);
+        pthread_mutex_lock(&mutex);
+        printf("消费者者%d进入缓冲区\n", consumer_id);
+        sem_wait(full_sem);
+        usleep(rand(100)*50000);
+        int i=findFull(product);
+        if(i!=-1){
+            printProduct(product);
+            product[i]=0;
+            printf("消费者%d在第%s个位置消费了产品\n", consumer_id, i);
+            printProduct(product);  
+        }
+        else{
+            sem_post(empty_sem);
+        }
+    }
 }
 int main(){
     pthread_mutex_init(&mutex);
     sem_init(&empty_sem,1,MAX_PRODUCT);
     sem_init(&full_sem,1,0);
-
+    for(int i=0;i<PRODUCTOR;i++){
+        pthread_t td;
+        pthread_create(&td, NULL, productor, i+1);
+    }
+    for(int i=0;i<CONSUMER;i++){
+        pthread_t td;
+        pthread_create(&td, NULL, consumer, i+1);
+    }
+    sleep(30);
+    return 0;
 }
